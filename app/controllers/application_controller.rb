35ffd97cf281e9363a814ff7c5e6da5a2d14ae51
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-24 23:44:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,6 +41,9 @@ class ApplicationController < Sinatra::Base
   end
 
   delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.destroy
+    @articles = Article.all
     erb :index
   end
 
